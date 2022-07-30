2575b16e4b3577662e86b03479e3d7a16d76d46a
Vaibhav Darvekar <vabdarwekar00@gmail.com>
2022-07-31 00:18:35 +0530

    animated dropdown added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -77,7 +77,7 @@ export const Navbar = () => {
               }}
             >
               <svg
-                className="w-6 h-6"
+                className="animate-bounce w-6 h-6 ..."
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -102,8 +102,22 @@ export const Navbar = () => {
                 setIsOpen(!isOpen);
               }}
             >
-              <svg
+              {/* <svg
                 className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 13l-7 7-7-7m14-8l-7 7-7-7"
+                ></path>
+              </svg> */}
+              <svg
+                className="animate-bounce w-6 h-6 ..."
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
